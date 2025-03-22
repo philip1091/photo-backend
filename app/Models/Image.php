d47ec21b41cd5f4105bdbfc7baa5631642ffa62a
HICINTUKA <hicintuka@Hicintukas-MacBook-Pro.local>
2025-03-22 17:01:39 -0400

    fix(api): compress the images have .webp as extension instead of jpg

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -32,31 +31,36 @@ class Image extends Model
                 $uploadedImage = request()->file('image');
 
                 $timestamp = time();
-                $compressedFileName = "images/compressed/{$timestamp}.jpg";
+                $compressedFileName = "images/compressed/{$timestamp}.webp"; // Store as WebP
 
                 $img = ImageProcessor::make($uploadedImage);
 
-                $maxFileSize = 80 * 1024;
-                $quality = 85;
+                $maxFileSize = 80 * 1024; // Target file size: 80 KB
+                $quality = 85; // Start with a high quality (85)
 
+                // Compress the image and reduce the quality to fit the target file size
                 do {
-                    $compressedImage = $img->encode('jpg', $quality);
+                    // Compress and convert to WebP
+                    $compressedImage = $img->encode('webp', $quality);
                     $quality -= 5;
-                } while (strlen($compressedImage) > $maxFileSize && $quality > 10);
+                } while (strlen($compressedImage) > $maxFileSize && $quality > 10); // Reduce quality until the size is acceptable
 
+                // Store the compressed WebP image in the 'public' storage disk
                 Storage::disk('public')->put($compressedFileName, (string) $compressedImage);
 
+                // Store the path of the compressed WebP image
                 $image->compressed_image = $compressedFileName;
             }
         });
 
         static::deleting(function ($image) {
             if ($image->compressed_image && Storage::disk('public')->exists($image->compressed_image)) {
-                Storage::disk('public')->delete($image->compressed_image);
+                Storage::disk('public')->delete($image->compressed_image); // Delete compressed image when record is deleted
             }
         });
     }
 
+    // This method is used to return the URL of the compressed image.
     public function getImagePathAttribute()
     {
         return Storage::url($this->compressed_image);
